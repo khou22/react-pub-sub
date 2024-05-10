50b61ff469d58cd68b9311d8dd8786c26c94ee24
Kevin Hou <k@exafunction.com>
2024-05-10 15:59:44 -0700

    Add components without functionality

---
+++ src/components/CounterDisplay/CounterDisplay.tsx
@@ -0,0 +1,8 @@
+export const CounterDisplay = () => {
+    return <div className="flex items-center justify-center border rounded-md p-4 shadow-md bg-gray-700 text-white min-w-[200px]">
+        <div className="text-4xl font-bold">
+            0
+        </div>
+    </div>;
+};
+
---
+++ src/components/IncrementButton/IncrementButton.tsx
@@ -0,0 +1,4 @@
+export const IncrementButton = () => {
+    return <button className="bg-gray-500 hover:bg-gray-600 text-white px-4 py-2 rounded-md">Increment</button>;
+};
+
